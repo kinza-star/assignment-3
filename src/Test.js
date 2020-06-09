8b20c937196602816d84f699b58c8deb47e1a272
kinzailyas <kinzailyasit@gmail.com>
2020-06-09 16:25:22 +0500

    update

---
+++ src/Test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+function Test(props) {
+    return (
+        <div>
+            <h1> hello {props.Name}</h1>
+            <h1> Mobile operating system </h1>
+            <ul>
+                <li>Android </li>
+                <li>Blackberry</li>
+                <li>iphone</li>
+                <li> Samsung</li>
+                
+            </ul>
+        </div>
+    );
+}
+export default Test;
